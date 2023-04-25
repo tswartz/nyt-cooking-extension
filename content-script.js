55715a4b14716082bb91d54a0cfe7ff2b2f8ae98
Talia Swartz <tswartz@hubspot.com>
2023-04-25 15:18:14 -0400

    Open HTML in new tab

--- content-script.js
+++ content-script.js
@@ -11,17 +11,37 @@ function revealRecipe() {
   if (modal) {
     modal.remove();
   }
+  const footers = document.querySelectorAll('footer');
+  footers.forEach(footer => footer.remove());
+  const iframes = document.querySelectorAll('iframe');
+  iframes.forEach(iframe => iframe.remove());
+  const scripts = document.querySelectorAll('script,link[as="script"]');
+  scripts.forEach(script => script.remove());
   const container = getContainer();
   if (container) {
     container.style.overflowY = 'scroll';
     container.style.height = '1000px';
   }
+
+  const html = document.createElement('html');
+  const head = document.createElement('head');
+  document.querySelectorAll('head meta,link[rel="stylesheet"],link[as="style"]').forEach(link => {
+    head.appendChild(link.cloneNode(true));
+  });
+  html.appendChild(head);
+  html.appendChild(document.querySelector('body').cloneNode(true));
+
+  const blob = new Blob([html.outerHTML], {
+    type: 'text/html'
+  });
+  const url = URL.createObjectURL(blob);
+  window.open(url);
 }
 
 function checkIfRecipeRevealed() {
   const modal = getModal();
   const container = getContainer();
-  return !modal && container.style.overflowY === 'scroll' && container.style.height;
+  return !modal && container && container.style.overflowY === 'scroll' && container.style.height;
 }
 
 chrome.runtime.onMessage.addListener(
